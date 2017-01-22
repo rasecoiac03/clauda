933fa072404c254461ff292454df7233a5693d61
Caio Silva <rasecoiac03@gmail.com>
2017-01-22 19:40:11 -0200

    fixing up things

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/rasecoiac03/clauda/pkg/config"
 )
 
-var fileFormat = "files/%s"
+var fileFolder = "files/"
 
 func main() {
 	downloadFiles()
@@ -20,21 +20,23 @@ func downloadFiles() {
 		"lotomania_file_enpoint",
 		"lotofacil_file_enpoint",
 	}
-	executeCmd("files folder exists", exec.Command("mkdir", "-p", fmt.Sprintf(fileFormat, "")))
-	executeCmd("files removed", exec.Command("rm", "-f", fmt.Sprintf(fileFormat, "*")))
+	executeCmd("files removed", exec.Command("rm", "-rf", fileFolder))
+	executeCmd("files folder exists", exec.Command("mkdir", "-p", fileFolder))
 	for _, file := range files {
 		filePath := config.GetConfig(file)
 		filePathParts := strings.Split(filePath, "/")
 		fileName := filePathParts[len(filePathParts)-1]
 		executeCmd(fmt.Sprintf("file downloaded: %s", fileName), exec.Command("wget", filePath))
-		executeCmd(fmt.Sprintf("file moved to folder: %s", fileName), exec.Command("mv", fileName, fmt.Sprintf(fileFormat, fileName)))
+		executeCmd(fmt.Sprintf("file unzipped: %s", fileName), exec.Command("unzip", "-u", fileName, "-d", fileFolder))
+		executeCmd(fmt.Sprintf("file zip removed: %s", fileName), exec.Command("rm", "-f", fileName))
+		executeCmd("cleaning", exec.Command("/bin/sh", "-c", "rm -f "+fileFolder+"*.GIF"))
 	}
 }
 
 func executeCmd(message string, cmd *exec.Cmd) {
-	defer log.Println(message)
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println(message)
 }
